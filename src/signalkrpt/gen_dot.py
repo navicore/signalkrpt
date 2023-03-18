04f03859e0eb840a3f4bab975e0b00662f67170f
Ed Sweeney <ed@onextent.com>
2023-03-18 10:14:11 -0700

    lint-refactor

--- src/signalkrpt/gen_dot.py
+++ src/signalkrpt/gen_dot.py
@@ -6,25 +6,52 @@ import json
 import math
 from collections import defaultdict
 
+def haversine_distance(coord1, coord2):
+    """
+    Function to calculate the distance between two coordinates
+    """
+    lat1, lon1 = coord1
+    lat2, lon2 = coord2
+    rad = 6371  # Earth radius in km
+
+    d_lat = math.radians(lat2 - lat1)
+    d_lon = math.radians(lon2 - lon1)
+    a_var = (math.sin(d_lat / 2) * math.sin(d_lat / 2) +
+         math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) *
+         math.sin(d_lon / 2) * math.sin(d_lon / 2))
+    cir = 2 * math.atan2(math.sqrt(a_var), math.sqrt(1 - a_var))
+    distance = rad * cir
+    return distance
+
+def create_dot_file_content(vessel_positions, vessel_names):
+    """
+    Add edges with dotted lines and lengths scaled to relative distances
+    """
+    # Generate the DOT file content for Graphviz
+    dot_file_content = 'digraph {\n'
+    dot_file_content += '  graph [splines=polyline]\n'
+
+    for vessel_id, position in vessel_positions.items():
+        dot_file_content += f'  "{vessel_id}" [label=\
+                "{vessel_names[vessel_id]}\\n({position[0]}, {position[1]})"];\n'
+
+
+    vessel_ids = list(vessel_positions.keys())
+    for i, id1 in enumerate(vessel_ids):
+        for _j, id2 in enumerate(vessel_ids[i + 1:], i + 1):
+            pos1 = vessel_positions[id1]
+            pos2 = vessel_positions[id2]
+            distance = haversine_distance(pos1, pos2)
+            dot_file_content += f'  "{id1}" -> "{id2}" [style=dotted, \
+                    len={distance / 10}, dir=none];\n'
+
+    dot_file_content += '}'
+    return dot_file_content
+
 def gen_dot(_args):
     """
     gen json for delta spec schema
     """
-
-    # Function to calculate the distance between two coordinates
-    def haversine_distance(coord1, coord2):
-        lat1, lon1 = coord1
-        lat2, lon2 = coord2
-        R = 6371  # Earth radius in km
-
-        dLat = math.radians(lat2 - lat1)
-        dLon = math.radians(lon2 - lon1)
-        a = (math.sin(dLat / 2) * math.sin(dLat / 2) +
-             math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) *
-             math.sin(dLon / 2) * math.sin(dLon / 2))
-        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-        distance = R * c
-        return distance
 
     # Read the Signal K data from stdin
     data = sys.stdin.read()
@@ -46,27 +73,8 @@ def gen_dot(_args):
         vessel_positions[vessel_id] = (lat, lon)
         vessel_names[vessel_id] = name
 
-    # Generate the DOT file content for Graphviz
-    dot_file_content = 'digraph {\n'
-    dot_file_content += '  graph [splines=polyline]\n'
-
-    for vessel_id, position in vessel_positions.items():
-        dot_file_content += f'  "{vessel_id}" [label="{vessel_names[vessel_id]}\\n({position[0]}, {position[1]})"];\n'
-
-    vessel_ids = list(vessel_positions.keys())
-
-    # Add edges with dotted lines and lengths scaled to relative distances
-    for i in range(len(vessel_ids)):
-        for j in range(i + 1, len(vessel_ids)):
-            id1 = vessel_ids[i]
-            id2 = vessel_ids[j]
-            pos1 = vessel_positions[id1]
-            pos2 = vessel_positions[id2]
-            distance = haversine_distance(pos1, pos2)
-            dot_file_content += f'  "{id1}" -> "{id2}" [style=dotted, len={distance / 10}, dir=none];\n'
-
-    dot_file_content += '}'
+    dot_file_content = create_dot_file_content(vessel_positions, vessel_names)
 
     # Write the DOT file content to a file
-    with open('vessel_positions.dot', 'w') as dot_file:
+    with open('vessel_positions.dot', 'w', encoding = 'utf8') as dot_file:
         dot_file.write(dot_file_content)
--- src/signalkrpt/gen_scatterplot.py
+++ src/signalkrpt/gen_scatterplot.py
@@ -14,20 +14,18 @@ def gen_scatterplot(_args):
 
     plt.figure()
 
-    for index, (vessel_id, vessel_info) in enumerate(vessels_data.items(), start=1):
+    for index, (_vessel_id, vessel_info) in enumerate(vessels_data.items(), start=1):
         if "navigation" in vessel_info and "position" in vessel_info["navigation"]:
             position = vessel_info["navigation"]["position"]["value"]
             latitude = position["latitude"]
             longitude = position["longitude"]
 
             plt.scatter(longitude, latitude)
-            plt.annotate(f"Boat {index}", (longitude, latitude), textcoords="offset points", xytext=(0, 5), ha='center')
+            plt.annotate(f"Boat {index}", (longitude, latitude),
+                textcoords="offset points", xytext=(0, 5), ha='center')
 
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
     plt.title("Boat Positions")
     plt.grid(True)
     plt.show()
-
-if __name__ == "__main__":
-    main()
